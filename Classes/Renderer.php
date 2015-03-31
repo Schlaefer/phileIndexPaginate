d74ce9d6448ed9aa737d2fc5582bb1685a61ea62
Schlaefer <openmail+sourcecode@siezi.com>
2015-03-31 12:02:31 +0200

    code formatting

--- Classes/Renderer.php
+++ Classes/Renderer.php
@@ -2,84 +2,96 @@
 
 namespace Phile\Plugin\Siezi\PhileIndexPaginate;
 
-class Renderer {
+class Renderer
+{
 
-    protected $settings = [];
+	protected $settings = [];
 
-    protected $renderer;
+	protected $renderer;
 
-    public function __construct($settings) {
-        $this->settings = $settings;
-        return $this;
-    }
+	public function __construct($settings)
+	{
+		$this->settings = $settings;
 
-    public function render(array $paginator) {
-        if (!$paginator) {
-            return false;
-        }
-        $out = [];
-        foreach ($paginator['items'] as $page) {
-            $out[] = $this->renderTemplate(
-              'content',
-              [
-                'current_page' => $page,
-                'meta' => $page->getMeta(),
-                'content' => $page->getContent(),
-              ]);
-        }
+		return $this;
+	}
 
-        $divider = $this->settings['templates']['divider'];
-        $out = implode($divider, $out);
+	public function render(array $paginator)
+	{
+		if (!$paginator) {
+			return false;
+		}
+		$out = [];
+		foreach ($paginator['items'] as $page) {
+			$out[] = $this->renderTemplate(
+				'content',
+				[
+					'current_page' => $page,
+					'meta' => $page->getMeta(),
+					'content' => $page->getContent(),
+				]);
+		}
 
-        $out .= $this->buildNavigation($paginator);
-        return $out;
-    }
+		$divider = $this->settings['templates']['divider'];
+		$out = implode($divider, $out);
 
-    protected function buildNavigation(array $paginator) {
-        $out = '';
+		$out .= $this->buildNavigation($paginator);
 
-        $makeLink = function ($page, $template) {
-            $link = $this->settings['uri'];
-            if ($page !== 1) {
-                $link .= '?page=' .  $page;
-            }
-            return $this->renderTemplate($template, ['link' => $link]);
-        };
+		return $out;
+	}
 
-        $current = $paginator['current'];
-        if (!$paginator['isFirst']) {
-            $out .= $makeLink($current - 1, 'previous');
-        }
-        if (!$paginator['isLast']) {
-            $out .= $makeLink($current + 1, 'next');
-        }
+	protected function buildNavigation(array $paginator)
+	{
+		$out = '';
 
-        return $out;
-    }
+		$makeLink = function ($page, $template) {
+			$link = $this->settings['uri'];
+			if ($page !== 1) {
+				$link .= '?page=' . $page;
+			}
 
-    protected function renderTemplate($template, array $vars = []) {
-        $template = $this->settings['templates'][$template];
-        if (strpos($template, 'file:') === 0) {
-            $options = ['base' => $this->settings['templateBasePath']];
-            $renderer = $this->getRenderer('file', $options);
-            $template = mb_substr($template, 5);
-        } else {
-            $renderer = $this->getRenderer('string');
-        }
-        return $renderer->render($template, $vars);
-    }
+			return $this->renderTemplate($template, ['link' => $link]);
+		};
 
-    protected function getRenderer($type, array $options = []) {
-        if (isset($this->renderer[$type])) {
-            return $this->renderer[$type];
-        }
-        if ($type === 'file') {
-            $loader = new \Twig_Loader_Filesystem($options['base']);
-        } else {
-            $loader = new \Twig_Loader_String();
-        }
-        $this->renderer[$type] = new \Twig_Environment($loader, ['autoescape' => false]);
-        return $this->renderer[$type];
-    }
+		$current = $paginator['current'];
+		if (!$paginator['isFirst']) {
+			$out .= $makeLink($current - 1, 'previous');
+		}
+		if (!$paginator['isLast']) {
+			$out .= $makeLink($current + 1, 'next');
+		}
+
+		return $out;
+	}
+
+	protected function renderTemplate($template, array $vars = [])
+	{
+		$template = $this->settings['templates'][$template];
+		if (strpos($template, 'file:') === 0) {
+			$options = ['base' => $this->settings['templateBasePath']];
+			$renderer = $this->getRenderer('file', $options);
+			$template = mb_substr($template, 5);
+		} else {
+			$renderer = $this->getRenderer('string');
+		}
+
+		return $renderer->render($template, $vars);
+	}
+
+	protected function getRenderer($type, array $options = [])
+	{
+		if (isset($this->renderer[$type])) {
+			return $this->renderer[$type];
+		}
+		if ($type === 'file') {
+			$loader = new \Twig_Loader_Filesystem($options['base']);
+		} else {
+			$loader = new \Twig_Loader_String();
+		}
+		$this->renderer[$type] = new \Twig_Environment($loader,
+			['autoescape' => false]);
+
+		return $this->renderer[$type];
+	}
 
 }
